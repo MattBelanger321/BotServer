50486cb4c0c4e410129eb28716ffad353bb48b1a
Matt Belanger <mattmbelanger@hotmail.com>
2020-07-31 00:17:18 -0400

    fixed duplication bug

--- Server/BotWebServer.java
+++ Server/BotWebServer.java
@@ -37,6 +37,7 @@ public class BotWebServer{
                 
                 dos.close();
                 dis.close();
+
             } catch(Exception e) {
                 e.printStackTrace();
             }
